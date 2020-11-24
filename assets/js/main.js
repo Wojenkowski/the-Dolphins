eb7d223718e1be08c47b244c079f970544cab6fb
Kamil Wojenkowski <kamil.wojenkowksi@student.howest.be>
2020-11-24 15:20:00 +0100

    add plug the power

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,7 @@ function showDatacenters(){
     let myHeaders = new Headers();
     myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9odGYyMDIwLnppbmRlcmxhYnMuY29tXC9hcGlcL2F1dGhcL2xvZ2luIiwiaWF0IjoxNjA2MjEwNzI2LCJleHAiOjE2MDYzOTA3MjYsIm5iZiI6MTYwNjIxMDcyNiwianRpIjoiNXBERjJIYjY5SWJ5Y3JCdiIsInN1YiI6OSwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9._ddCBmnzRZ-DZDm5Lgt03xryVhqU7S935FNqg_Y-6X0");
 
+
     let requestOptions = {
         method: 'GET',
         headers: myHeaders,
@@ -24,13 +25,19 @@ function showDatacenters(){
         .then(function (data) {
             let html = " ";
             data.data.forEach(function (item){
-                html = "<tr><td>"+item.id +"</td><td>"+item.name +"</td><td>"+item.location.lng +"</td> <td>"+item.location.lng +"</td><td>"+item.provider +"</td><td>"+item.inIsolation +"</td><td><button id="+item.id+" type=\"button\" class=\"btn btn-primary\">show errors</button></td></th></tr>";
+                html = "<tr><td>"+item.id +"</td><td>"+item.name +"</td><td>"+item.location.lng +"</td> <td>"+item.location.lng +"</td><td>"+item.provider +"</td><td>"+item.inIsolation +"</td><td><button id="+item.id+" type=\"button\" class=\"btn btn-primary errors\">show errors</button></td><td><button id="+item.id+" type=\"button\" class=\"btn btn-danger power\">Plug the power</button></th></tr>";
                 document.getElementsByTagName("tbody")[0].innerHTML += html;
             });
-            let strains = document.querySelectorAll('button');
+            let strains = document.getElementsByClassName("errors") ;
 
             for (let i = 0; i < strains.length; i++) {
                 strains[i].addEventListener('click', nextpage);
+            }
+
+            let power = document.getElementsByClassName("power") ;
+
+            for (let i = 0; i < power.length; i++) {
+                power[i].addEventListener('click', isolate);
             }
         }).catch(function (err) {
         console.log(err)
@@ -38,8 +45,30 @@ function showDatacenters(){
 }
 
 function nextpage(e){
-    console.log(e.target.id);
    window.location.href = "errors.html?id="+e.target.id;
+}
+function isolate(e){
+    console.log(e.target.id);
+
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9odGYyMDIwLnppbmRlcmxhYnMuY29tXC9hcGlcL2F1dGhcL2xvZ2luIiwiaWF0IjoxNjA2MjEwNzI2LCJleHAiOjE2MDYzOTA3MjYsIm5iZiI6MTYwNjIxMDcyNiwianRpIjoiNXBERjJIYjY5SWJ5Y3JCdiIsInN1YiI6OSwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9._ddCBmnzRZ-DZDm5Lgt03xryVhqU7S935FNqg_Y-6X0");
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch('https://htf2020.zinderlabs.com/api/datacenters/'+e.target.id+'/isolate', requestOptions)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (result) {
+            console.log(result);
+        }).catch(function (err) {
+        console.log(err)
+    });
+
 }
 
 
